0298dccab5e1557ba5fe53346116f7a971dd1c1e
Kozlov Taras <kozlov.taras@gmail.com>
2012-04-11 22:37:12 +0400

    Set build type to release

--- build.cmd
+++ build.cmd
@@ -2,13 +2,13 @@
 call git clone https://github.com/zeromq/zeromq2-x.git
 call git clone https://github.com/zeromq/jzmq.git
 
-copy /Y zeromq.cmake zeromq2-x/CMakeLists.txt
-copy /Y jzmq.cmake jzmq/CMakeLists.txt
+cmake -E copy zeromq.cmake zeromq2-x/CMakeLists.txt
+cmake -E copy jzmq.cmake jzmq/CMakeLists.txt
 
 mkdir build%Platform%
 cd build%Platform%
 
-call cmake -G "NMake Makefiles" ..
+call cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=stage -G "NMake Makefiles" ..
 call nmake package
 
 copy /Y zmq-private* ..
