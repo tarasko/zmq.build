ce10a856558415fb0e8ab595eb12e3c13ca0c209
Kozlov Taras <kozlov.taras@gmail.com>
2012-04-13 14:06:16 +0400

    Simplify build structure

--- build.cmd
+++ build.cmd
@@ -1,12 +1,10 @@
 @echo off
 
-call checkoutandpatch.cmd
-
 mkdir build%Platform%
 cd build%Platform%
 
 call cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=stage -G "NMake Makefiles" ..
 call nmake package
 
-copy /Y zmq-private* ..
+copy /Y zmq.build* ..
 cd ..
--- checkoutandpatch.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-call git clone https://github.com/zeromq/zeromq2-x.git
-call git clone https://github.com/zeromq/jzmq.git
-
-cmake -E copy zeromq.cmake zeromq2-x/CMakeLists.txt
-cmake -E copy jzmq.cmake jzmq/CMakeLists.txt
