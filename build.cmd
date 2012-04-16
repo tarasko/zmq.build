2243959e39ddee51faf543c024ef3c0469a5e360
Kozlov Taras <kozlov.taras@gmail.com>
2012-04-16 14:48:47 +0400

    Don`t compile old examples

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 mkdir build%Platform%
 cd build%Platform%
 
-call cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=stage -G "NMake Makefiles" ..
+call cmake -DSKIP_EXAMPLES=1 -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=stage -G "NMake Makefiles" ..
 call nmake package
 
 copy /Y zmq.build* ..
